0a73c0dcae8b74ae18cfb47599c4d9e8fba2c536
safdar211ali <safdar211@hotmail.com>
2024-07-20 11:10:18 +0500

    updated

--- yofroyo/hooks.py
+++ yofroyo/hooks.py
@@ -13,7 +13,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/yofroyo/css/yofroyo.css"
 # app_include_js = "/assets/yofroyo/js/yofroyo.js"
-# app_include_js = "/assets/yofroyo/js/point.js"
+app_include_js = "/assets/yofroyo/js/point-of-sale.min.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/yofroyo/css/yofroyo.css"
